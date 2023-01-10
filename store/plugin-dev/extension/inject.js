2cd5b6bba2f1a1e8bfe4851a1b2d48ac4703de63
codehard <sarawut.k@codehard.co.th>
2023-01-10 11:57:49 +0700

    edit URL_TO_PLUGIN

--- store/plugin-dev/extension/inject.js
+++ store/plugin-dev/extension/inject.js
@@ -1,5 +1,5 @@
 (function(window, undefined) {
-	var URL_TO_PLUGIN = "";
+	var URL_TO_PLUGIN = "https://github.com/whitedevth/onlyoffice.github.io/tree/master/sdkjs-plugins/content/exshortcut";
 	
 	var xhrObj = new XMLHttpRequest();
 	xhrObj.open('GET', URL_TO_PLUGIN + "config.json", false);
