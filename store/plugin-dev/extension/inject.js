543776bc61d57a92a68c6e8f634b9c2d6010a71c
codehard <sarawut.k@codehard.co.th>
2023-01-16 14:07:47 +0700

    .

--- store/plugin-dev/extension/inject.js
+++ store/plugin-dev/extension/inject.js
@@ -1,6 +1,6 @@
-(function(window, undefined) {
-	var URL_TO_PLUGIN = "https://cors-anywhere.herokuapp.com/https://github.com/whitedevth/onlyoffice.github.io/tree/master/sdkjs-plugins/content/exshortcut";
-	
+(function (window, undefined) {
+	var URL_TO_PLUGIN = "";
+
 	var xhrObj = new XMLHttpRequest();
 	xhrObj.open('GET', URL_TO_PLUGIN + "config.json", false);
 	xhrObj.send('');
@@ -11,9 +11,8 @@
 	window.Asc = window.Asc ? window.Asc : {};
 	window.Asc.extensionPlugins = window.Asc.extensionPlugins ? window.Asc.extensionPlugins : [];
 	window.Asc.extensionPlugins.push(configObj);
-	if (window.Asc.g_asc_plugins && window.Asc.g_asc_plugins.loadExtensionPlugins)
-	{
+	if (window.Asc.g_asc_plugins && window.Asc.g_asc_plugins.loadExtensionPlugins) {
 		window.Asc.g_asc_plugins.loadExtensionPlugins(window.Asc.extensionPlugins);
 		window.Asc.extensionPlugins = [];
-	}	
+	}
 })(window, undefined);
